eea29fb9eb2aced0b3b2dad5357cd86cc8f785ae
majo <mariajoseaguilar188@gmail.com>
2026-05-24 20:29:59 -0500

    fix cors allowedOriginPatterns

--- src/main/java/com/habittracker/habittracker_api/CorsConfig.java
+++ src/main/java/com/habittracker/habittracker_api/CorsConfig.java
@@ -10,11 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                    "http://localhost:5173",
-                    "http://localhost:4173",
-                    "https://habittracker-frontend-nu.vercel.app"
-                )
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
