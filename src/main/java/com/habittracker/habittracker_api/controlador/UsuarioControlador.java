b49189be16ff92b7ce15b8d147410ff036099d18
majo <mariajoseaguilar188@gmail.com>
2026-05-24 20:33:46 -0500

    quitar CrossOrigin del controlador

--- src/main/java/com/habittracker/habittracker_api/controlador/UsuarioControlador.java
+++ src/main/java/com/habittracker/habittracker_api/controlador/UsuarioControlador.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/usuarios")
-@CrossOrigin(origins = "*")
 public class UsuarioControlador {
 
     @Autowired
